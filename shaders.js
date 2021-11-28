c90b68d7a356de5b2ddae2c24472b489ae997cde
Clément Geiger <clement.geiger@datadoghq.com>
2021-11-28 01:02:41 +0100

    backticks are nice

--- shaders.js
+++ shaders.js
@@ -1,22 +1,22 @@
-const vertexShaderSource =
-  "    attribute vec3 vertexPos;\n" +
-  "    attribute vec2 vertexTexCoord;\n" +
-  "    uniform mat4 modelViewMatrix;\n" +
-  "    uniform mat4 projectionMatrix;\n" +
-  "    varying vec2 vTexCoord;\n" +
-  "    void main(void) {\n" +
-  "		     // Return the transformed and projected vertex value\n" +
-  "        gl_Position = projectionMatrix * modelViewMatrix * \n" +
-  "            vec4(vertexPos, 1.0);\n" +
-  "        // Output the vertexColor in vColor;\n" +
-  "        vTexCoord = vertexTexCoord;\n" +
-  "    }\n";
+const vertexShaderSource = `
+    attribute vec3 vertexPos;
+    attribute vec2 vertexTexCoord;
+    uniform mat4 modelViewMatrix;
+    uniform mat4 projectionMatrix;
+    varying vec2 vTexCoord;
+    void main(void) {
+        // Return the transformed and projected vertex value
+        gl_Position = projectionMatrix * modelViewMatrix *
+        vec4(vertexPos, 1.0);
+        // Output the vertexColor in vColor;
+        vTexCoord = vertexTexCoord;
+    }`;
 
-const fragmentShaderSource =
-  "    precision mediump float;\n" +
-  "    varying vec2 vTexCoord;\n" +
-  "    uniform sampler2D uSampler;\n" +
-  "    void main(void) {\n" +
-  "        // Return the pixel color: always output white\n" +
-  "        gl_FragColor = texture2D(uSampler, vec2(vTexCoord.s, vTexCoord.t));\n" +
-  "}\n";
+const fragmentShaderSource = `
+    precision mediump float;
+    varying vec2 vTexCoord;
+    uniform sampler2D uSampler;
+    void main(void) {
+        // Return the pixel color: always output white
+        gl_FragColor = texture2D(uSampler, vec2(vTexCoord.s, vTexCoord.t));
+    }`;
